117c56faee80140a4df22b6bdfa4179ba9611054
MD. Faysal Rahman <faysal35-3119@diu.edu.bd>
2023-02-06 23:42:01 +0600

    1

---
+++ Ostad/exam.php
@@ -0,0 +1,12 @@
+<?php
+
+function comCalculator($tutFee)
+{
+    //    $tutionFee = 1234;
+    $calculator = (20000 <= $tutFee) ? $tutFee * 25 / 100 : ((10000 <= $tutFee && 20000 >= $tutFee) ? $tutFee * 20 / 100 : ((10000 >= $tutFee && 7000 <= $tutFee) ? $tutFee * 15 / 100 : ((7000 >= $tutFee) ? "0" : false)));
+    echo "commission is for " . $tutFee . " is = " . $calculator;
+    //
+
+}
+
+echo (comCalculator(5000));
