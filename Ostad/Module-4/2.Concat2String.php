627e817595cfaa7bec41abdeeb883720db08d501
MD. Faysal Rahman <faysal35-3119@diu.edu.bd>
2023-02-20 22:16:24 +0600

    revision

--- Ostad/Module-4/2.Concat2String.php
+++ Ostad/Module-4/2.Concat2String.php
@@ -3,19 +3,13 @@
 
 function concatenateFromEnd($str1, $str2)
 {
-    // $str1Len = strlen($str1);
-    // $str2Len = strlen($str2);
-    // if ($str1 === null) {
-    //     return $str2;
-    // }
-    // if ($str2 === null) {
-    //     return $str1;
-    // }
+
     return $str1 . $str2;
 }
 
 
-$str1 = '';
+$str1 = 'Hello ';
 $str2 = 'World';
 $result = concatenateFromEnd($str1, $str2);
-echo $result; // outputs 'HellWorld'
+echo $result; 
+// outputs 'HellWorld'
